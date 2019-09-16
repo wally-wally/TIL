84bb9820c988eeeecfaf538ebfdbc4b88f59ae7a
wally-wally <wallys0213@gmail.com>
2019-09-17 01:15:30 +0900

    190917 | add SWEA_AD_Exhaustive_search

--- 02_algorithm/09.Exhaustive_search,Greedy/1865.py
+++ 02_algorithm/09.Exhaustive_search,Greedy/1865.py
@@ -1,46 +1,26 @@
 import sys
 sys.stdin = open('input_1865.txt', 'r')
 
-def probability(data):
-    global probability_value
-    result = 1
-    for n in range(N):
-        if arr[n][data[n]] != 0:
-            result *= (arr[n][data[n]] / 100)
-            if result < probability_value:
-                return
-    else:
-        probability_value = result
-
-# def perm(k, n, used):
-#     if k == n:
-#         probability(order)
-#     for i in range(n):
-#         if used & (1 << i): continue
-#         order.append(number_list[i])
-#         perm(k + 1, n, used | (1 << i))
-#         order.pop()
-
-# import itertools
-# def perm(k):
-#     for element in itertools.permutations(number_list):
-#         probability(list(element))
-
-# def perm(k):
-#     if k == N:
-#         # print(number_list)
-#         probability(number_list)
-#     else:
-#         for i in range(k, N):
-#             number_list[k], number_list[i] = number_list[i], number_list[k]
-#             perm(k + 1)
-#             number_list[k], number_list[i] = number_list[i], number_list[k]
+def probability(people_num, val):
+    global ans, N
+    if val <= ans:
+        return
+    if people_num == N:
+        if val >= ans:
+            ans = val
+            return
+    for i in range(N):
+        if not used[i]:
+            used[i] = True
+            probability(people_num + 1, val * (arr[people_num][i] / 100))
+            used[i] = False
 
 for test_case in range(int(input())):
-    probability_value = 0
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
     number_list = [num for num in range(N)]
-    order = []
-    perm(0, N, 0)
-    print('#{} {:0<9}'.format(test_case + 1, round(probability_value * 100, 6)))
+    used = [False] * N
+    people_n, ans = 0, 0
+    probability_value = 1 # 재귀 함수를 통해 계속 곱해질 확률 값이므로 1로 초기값 선언
+    probability(people_n, probability_value)
+    print('#{} {:.6f}'.format(test_case + 1, ans * 100))
