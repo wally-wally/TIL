19b6350692e756a011a9a31e0120cbb86082696e
wally-wally <wallys0213@gmail.com>
2019-09-23 17:54:28 +0900

    190923 | revise code_SWEA_1486

--- 02_algorithm/sw_expert_academy/code_problem/D4/1486.장훈이의 높은 선반/1486.py
+++ 02_algorithm/sw_expert_academy/code_problem/D4/1486.장훈이의 높은 선반/1486.py
@@ -1,13 +1,8 @@
 import sys
-sys.stdin = open('input.txt', 'r')
+sys.stdin = open('input_1486.txt', 'r')
 
-T = int(input())
-
-for i in range(T):
-    result = 0
-    num = list(map(int, input().split()))
-    clerk_count = num[0] # 점원의 수
-    janghoon_tall = num[1] # 장훈이의 키
+for i in range(int(input())):
+    clerk_count, janghoon_tall = map(int, input().split()) # 점원의 수, 장훈이의 키
     clerk_tall = list(map(int, input().split())) # 점원들의 키 list
 
     clerk_tower_max = 1000000000 # 점원읖 높이 세운 탑의 높이를 임의로 아주 큰 값으로 설정
