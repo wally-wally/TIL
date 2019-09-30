221e7ee67e4f4368b5869930d93adce9dd6250f3
wally-wally <wallys0213@gmail.com>
2019-09-30 13:38:10 +0900

    190930 | change code SWEA_1247

--- 02_algorithm/sw_expert_academy/code_problem/all_problem/1247.py
+++ 02_algorithm/sw_expert_academy/code_problem/all_problem/1247.py
@@ -1,7 +1,50 @@
 import sys
 sys.stdin = open('input_1247.txt', 'r')
 
-# 1. itertools 모듈로 만든 순열 사용
+# 1. 재귀 함수를 이용해 만든 순열 이용
+def perm(k, distance_value):
+    global result, order
+    if distance_value > result:
+        return
+    if k == customer_cnt + 1:
+        distance_value += abs(position_list[-1][0] - position_list[order[-1]][0]) + abs(position_list[-1][1] - position_list[order[-1]][1])
+        if distance_value <= result:
+            result = distance_value
+        return
+    else:
+        for i in range(1, customer_cnt + 1):
+            if not visited[i]:
+                visited[i] = 1
+                order.append(customer_idx[i - 1])
+                if len(order) == 1:
+                    perm(k + 1, distance_value + abs(position_list[0][0] - position_list[order[0]][0]) + abs(position_list[0][1] - position_list[order[0]][1]))
+                else:
+                    perm(k + 1, distance_value + abs(position_list[order[-2]][0] - position_list[order[-1]][0]) + abs(position_list[order[-2]][1] - position_list[order[-1]][1]))                    
+                visited[i] = 0
+                order.pop()
+
+for test_case in range(int(input())):
+    customer_cnt = int(input())
+    customer_idx = [num for num in range(1, customer_cnt + 2)]
+    position_info = list(map(int, input().split()))
+    position_list = [] # 회사 - 고객들 - 집 순으로 위치 저장함
+    for idx in range(customer_cnt + 2):
+        if idx == 0:
+            company = (position_info[idx * 2], position_info[idx * 2 + 1])
+        elif idx == 1:
+            home = (position_info[idx * 2], position_info[idx * 2 + 1])
+        else:
+            position_list.append((position_info[idx * 2], position_info[idx * 2 + 1]))
+    position_list.insert(0, company)
+    position_list.append(home)
+    result = 10**8
+    visited = [0] * (customer_cnt + 2)
+    order = []
+    perm(1, 0)
+    print('#{} {}'.format(test_case + 1, result))
+
+# 2. itertools 모듈로 만든 순열 사용
+'''
 import itertools
 
 for test_case in range(int(input())):
@@ -35,57 +78,5 @@ for test_case in range(int(input())):
             comb_length += 1
         if distance_value <= result:
             result = distance_value
-    print('#{} {}'.format(test_case + 1, result))
-
-
-# 2. 재귀 함수를 이용해 만든 순열 이용
-'''
-def perm(k):
-    global result, order
-    if k == customer_cnt + 1:
-        distance_value = 0
-        comb_length = 0
-        order = order + [customer_cnt + 1]
-        for i in range(customer_cnt + 1):
-            if distance_value > result:
-                break
-            if comb_length == 0:
-                distance_value += abs(position_list[0][0] - position_list[order[i]][0]) + abs(position_list[0][1] - position_list[order[i]][1])
-            elif comb_length == customer_cnt + 1:
-                distance_value += abs(position_list[-1][0] - position_list[order[i]][0]) + abs(position_list[-1][1] - position_list[order[i]][1])
-            else:
-                distance_value += abs(position_list[order[i - 1]][0] - position_list[order[i]][0]) + abs(position_list[order[i - 1]][1] - position_list[order[i]][1])
-            comb_length += 1
-        if distance_value <= result:
-            result = distance_value
-        order.pop()
-        return
-    else:
-        for i in range(1, customer_cnt + 1):
-            if not visited[i]:
-                visited[i] = 1
-                order.append(customer_idx[i - 1])
-                perm(k + 1)
-                visited[i] = 0
-                order.pop()
-
-for test_case in range(int(input())):
-    customer_cnt = int(input())
-    customer_idx = [num for num in range(1, customer_cnt + 2)]
-    position_info = list(map(int, input().split()))
-    position_list = [] # 회사 - 고객들 - 집 순으로 위치 저장함
-    for idx in range(customer_cnt + 2):
-        if idx == 0:
-            company = (position_info[idx * 2], position_info[idx * 2 + 1])
-        elif idx == 1:
-            home = (position_info[idx * 2], position_info[idx * 2 + 1])
-        else:
-            position_list.append((position_info[idx * 2], position_info[idx * 2 + 1]))
-    position_list.insert(0, company)
-    position_list.append(home)
-    result = 10**8
-    visited = [0] * (customer_cnt + 2)
-    order = []
-    perm(1)
     print('#{} {}'.format(test_case + 1, result))
 '''
