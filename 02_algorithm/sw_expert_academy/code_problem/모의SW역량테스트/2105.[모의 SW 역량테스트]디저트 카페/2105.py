937741f6df92f962d8181ab129236e508b635326
wally-wally <wallys0213@gmail.com>
2019-11-13 23:20:10 +0900

    191113 | update vue_practice, add SWEA_2105

---
+++ 02_algorithm/sw_expert_academy/code_problem/모의SW역량테스트/2105.[모의 SW 역량테스트]디저트 카페/2105.py
@@ -0,0 +1,35 @@
+import sys
+sys.stdin = open('input_2105.txt', 'r')
+
+def dessert_check(r, c, des_cnt, rot_cnt):
+    global result
+    new_r, new_c = r + dx[rot_cnt], c + dy[rot_cnt]
+    if rot_cnt == 3 and new_r == init_row and new_c == init_col:
+        result = max(result, des_cnt)
+        return
+    if 0 <= new_r < N and 0 <= new_c < N:
+        if desserts[cafe_map[new_r][new_c]] == 0 and visited[new_r][new_c] == 0:
+            desserts[cafe_map[new_r][new_c]] = 1
+            visited[new_r][new_c] = 1
+            dessert_check(new_r, new_c, des_cnt + 1, rot_cnt)
+            if rot_cnt < 3:
+                dessert_check(new_r, new_c, des_cnt + 1, rot_cnt + 1)
+            visited[new_r][new_c] = 0
+            desserts[cafe_map[new_r][new_c]] = 0
+
+for tc in range(int(input())):
+    N = int(input())
+    cafe_map = [list(map(int, input().split())) for _ in range(N)]
+    visited = [[0] * N for _ in range(N)]
+    desserts = [0] * 101
+    dx, dy = [-1, -1, +1, +1], [-1, +1, +1, -1] # 대각 4방향
+    result = 0
+    for idx in range(N * N):
+        row, col = idx // N, idx % N
+        init_row, init_col = row, col # 초기 상태 저장
+        desserts[cafe_map[row][col]] = 1
+        visited[row][col] = 1
+        dessert_check(row, col, 1, 0) # 세 번째 인자 : 조건에 맞는 지금 까지 구한 디저트 개수, 네 번재 인자 : 90도 꺾는 횟수
+        visited[row][col] = 0
+        desserts[cafe_map[row][col]] = 0
+    print('#{} {}'.format(tc + 1, result if result else -1))
---
+++ 07_vue/vue_practice/README.md
@@ -0,0 +1,18 @@
+# :keyboard: Vue_practice Contents
+
+<br>
+
+## :one: `01_todo_list`
+
+| 파일명              | 내용                                                         |
+| ------------------- | ------------------------------------------------------------ |
+| `01_todo_list.html` | <ul><li>binding 기초</li><li>el, data, methods</li><li>v-for, v-if, v-bind</li></ul> |
+| `02_todo_list.html` | <ul><li>새로운 항목 추가하기 버튼 생성</li><li>완료 항목 삭제하기 버튼 생성</li></ul> |
+| `03_todo_list.html` | <ul><li>v-model</li><li>style binding</li><li>class binding</li></ul> |
+| `04_todo_list.html` | <ul><li>category 만들기</li><li>리스트 형태 => 체크박스 형태 변경</li></ul> |
+| `05_todo_list.html` | <ul><li>computed</li></ul>                                   |
+| `06_todo_list.html` | <ul><li>local storage</li><li>watch</li><li>mounted</li><li>체크박스 형태 => 리스트 형태 변경</li></ul> |
+| `07_todo_list.html` | <ul><li>각각의 todo에 개별 id를 부여</li><li>특정 todo를 삭제하도록 기능 추가</li></ul> |
+| `08_todo_list.html` | <ul><li>Component</li></ul>                                  |
+| `09_todo_list.html` | <ul><li>props</li></ul>                                      |
+
