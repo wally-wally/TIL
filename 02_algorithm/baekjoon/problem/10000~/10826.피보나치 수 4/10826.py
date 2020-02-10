477736a39ec4136ec148134c5aa6650d849d8d3a
wally-wally <wallys0213@gmail.com>
2020-02-10 23:46:19 +0900

    200210 | add BOJ_problem

---
+++ 02_algorithm/baekjoon/problem/10000~/10826.피보나치 수 4/10826.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('input_10826.txt', 'r')
+
+def fibo(n):
+    fibo_array = [0, 1]
+    for num in range(2, n + 1):
+        fibo_array.append(fibo_array[num - 2] + fibo_array[num - 1])
+    return fibo_array[-1]
+
+n = int(input())
+print(n if n <= 1 else fibo(n))
