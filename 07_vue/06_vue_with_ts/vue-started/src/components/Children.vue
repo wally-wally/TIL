75e5dfd7514d8b2317cb24d2fe02ace430cf3efd
wally-wally <wallys0213@gmail.com>
2020-05-21 02:37:32 +0900

    200521 | add 07_06_vue_with_ts

---
+++ 07_vue/06_vue_with_ts/vue-started/src/components/Children.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ parentMessage }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Children extends Vue {
+  // tsconfig.json에서 strict 모드를 true로 되어 있는데
+  // undefined를 포함하지 않는 class 속성은 반드시 초기화 되어야 한다는 규칙을 포함하고 있다.
+  // 그래서 parentMessage 값을 초기화 해버리면 부모에서 넘겨준 parentMessage 값이 초기화 값으로 덮어씌워져 버리기 때문에
+  // ?를 붙여줌으로써 parentMessage를 optional 하게 설정해야 한다.
+  @Prop() parentMessage?: string;
+  // 그러면 아래와 같은 오류 안 뜸
+  // Property 'parentMessage' has no initializer and is not definitely assigned in the constructor.
+}
+</script>
---
+++ 07_vue/06_vue_with_ts/vue-started/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Message></Message>
+    <hr>
+    <Children :parentMessage="message"></Children>
+  </div>
+</template>
+
+<script lang="ts">
+// @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator';
+import Message from '@/components/Message.vue';
+import Children from '@/components/Children.vue';
+
+@Component({
+  components: {
+    Children,
+    Message,
+  },
+})
+export default class Home extends Vue {
+  // 동적인 데이터 props로 넘기는 방법
+  // template 단에서 :을 붙이고 변수명을 작성해서 데이터를 넘기면 된다.
+  message: string = 'hello world';
+}
+</script>
