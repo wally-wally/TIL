1befc7143ae3df8f55f037870b4f70fce35937cf
wally-wally <wallys0213@gmail.com>
2020-07-20 16:26:27 +0900

    200720 | add 03_styling-react before yarn eject

---
+++ 08_react/01_react_jsx/03_styling-react/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SassComponent from './SassComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
