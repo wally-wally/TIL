577d1b904962a5729a8ab8118d326525f873211f
wally-wally <wallys0213@gmail.com>
2020-11-04 00:02:20 +0900

    201104 | add todo_quiz typescript

---
+++ 09_ts/02_captain/quiz/1_todo/src/filterAPI.js
@@ -0,0 +1,13 @@
+let arr = [
+  { gender: 'male', name: 'john' },
+  { gender: 'female', name: 'sara' },
+  { gender: 'male', name: 'bone' },
+];
+
+let filtered = arr.filter(function(item) {
+  if (item.gender === 'female') {
+    return item;
+  }
+})
+
+console.log(filtered);
---
+++ 09_ts/02_captain/quiz/1_todo/src/index.ts
@@ -0,0 +1,83 @@
+// 타입 별칭
+// type todo = {
+//   id: number,
+//   title: string,
+//   done: boolean
+// };
+
+// 인터페이스
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+let todoItems: Todo[];
+
+// api
+function fetchTodoItems(): Todo[] {
+  const todos = [
+    { id: 1, title: '안녕', done: false },
+    { id: 2, title: '타입', done: false },
+    { id: 3, title: '스크립트', done: false },
+  ];
+  return todos;
+}
+
+// crud methods
+function fetchTodos(): object[] {
+  const todos = fetchTodoItems();
+  return todos;
+}
+
+function addTodo(todo: Todo): void {
+  todoItems.push(todo);
+}
+
+function deleteTodo(index: number): void {
+  todoItems.splice(index, 1);
+}
+
+function completeTodo(index: number, todo: Todo): void {
+  todo.done = true;
+  todoItems.splice(index, 1, todo);
+}
+
+// business logic
+function logFirstTodo(): object {
+  return todoItems[0];
+}
+
+function showCompleted() {
+  return todoItems.filter(item => item.done);
+  // return todoItems.filter(function(item) {
+  //   if (item.done) {
+  //     return item;
+  //   }
+  // })
+}
+
+// TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
+function addTwoTodoItems(): void {
+  // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+  const item1 = {
+    id: 4,
+    title: 'item 4',
+    done: false
+  };
+  addTodo(item1);
+  addTodo({
+    id: 5,
+    title: 'item 5',
+    done: false,
+  });
+}
+
+// NOTE: 유틸 함수
+function log(): void {
+  console.log(todoItems);
+}
+
+todoItems = fetchTodoItems();
+addTwoTodoItems();
+log();
