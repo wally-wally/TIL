80e5f83734c5425a0ba83faf6ee3cb04c52bedb9
wally-wally <wallys0213@gmail.com>
2020-11-17 00:03:56 +0900

    201117 | add AddBoard, Modal components at vue_trello

--- 07_vue/07_vue_trello/lecture-vue-trello/src/components/Home.vue
+++ 07_vue/07_vue_trello/lecture-vue-trello/src/components/Home.vue
@@ -1,54 +1,113 @@
 <template>
   <div>
-    Home
-    <div>
-      Board List:
-      <div v-if="loading">Loading...</div>
-      <div v-else>
-        <div v-for="b in boards" :key="b.id">
-          {{ b }}
-        </div>
+    <div class="home-title">Personal Boards</div>
+    <div class="board-list" ref="boardList">
+      <div class="board-item" v-for="b in boards" :key="b.id" 
+        :data-bgcolor="b.bgColor" ref="boardItem">
+        <router-link :to="`/b/${b.id}`">
+          <div class="board-item-title">{{b.title}}</div>
+        </router-link>
       </div>
-      <ul>
-        <li>
-          <router-link to="/b/1">Board 1</router-link>
-        </li>
-        <li>
-          <router-link to="/b/2">Board 2</router-link>
-        </li>
-      </ul>
+      <div class="board-item board-item-new">
+        <a class="new-board-btn" href="" @click.prevent="addBoard">
+          Create new board...
+        </a>
+      </div>
     </div>
+    <AddBoard v-if="isAddBoard" @close="isAddBoard=false" @submit="onAddBoard" />
   </div>
 </template>
 
 <script>
 import { board } from '../api'
+import AddBoard from './AddBoard.vue'
 
 export default {
+  components: {
+    AddBoard
+  },
   data() {
     return {
       loading: false,
-      boards: []
+      boards: [],
+      error: '',
+      isAddBoard: false
     }
   },
   created() {
     this.fetchData()
+  },
+  updated() {
+    this.$refs.boardItem.forEach(el => {
+      el.style.backgroundColor = el.dataset.bgcolor
+    })
   },
   methods: {
     fetchData() {
       this.loading = true
       board.fetch()
         .then(data => {
-          this.boards = data
+          this.boards = data.list
         })
-        .finally(_ => {
+        .finally(_=> {
           this.loading = false
         })
+    },
+    addBoard() {
+      this.isAddBoard = true
+    },
+    onAddBoard(title) {
+      board.create(title)
+        .then(() => this.fetchData())
     }
-  },
+  }
 }
 </script>
 
 <style>
-
+.home-title {
+  padding: 10px;
+  font-size: 18px;
+  font-weight: bold;
+}
+.board-list {
+  padding: 10px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.board-item {
+  width: 23%;
+  height: 100px;
+  margin: 0 2% 20px 0;
+  border-radius: 3px;
+}
+.board-item-new {
+  background-color: #ddd;
+}
+.board-item a {
+  text-decoration: none;
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+.board-item a:hover,
+.board-item a:focus {
+  background-color: rgba(0,0,0, .1);
+  color: #666;
+}
+.board-item-title {
+  color: #fff;
+  font-size: 18px;
+  font-weight: 700;
+  padding: 10px;
+}
+.board-item a.new-board-btn {
+  display: table-cell;
+  vertical-align: middle;
+  text-align: center;
+  height: 100px;
+  width: inherit;
+  color: #888;
+  font-weight: 700;
+}
 </style>
