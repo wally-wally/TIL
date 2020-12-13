2c497edbb53dd74fa06ac70d3fdede3659de1c7e
wally-wally <wallys0213@gmail.com>
2020-12-13 20:48:55 +0900

    201213 | delete unnecessary files

--- 02_algorithm/SSAFY_DP/[BOJ]9095.1, 2, 3 더하기/9095.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.stdin = open('input_9095.txt', 'r')
-
-for _ in range(int(input())):
-    n = int(input())
-    DP = [0, 1, 2, 4] + [0] * (n - 3)
-    for i in range(4, n + 1):
-        DP[i] = sum(DP[i - 3 : i])
-    print(DP[n])
