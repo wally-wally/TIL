25b343b3b335ba0807a2c81ccf53a8fc84c027e7
wally-wally <wallys0213@gmail.com>
2020-12-20 10:48:40 +0900

    201220 | delete unnecessary files

--- 04_django/CRUD_TEMP/jobs/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'jobs'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('past_life/', views.past_life, name='past_life'),
-]
