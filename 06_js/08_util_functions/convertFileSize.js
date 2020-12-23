37564757e8db67010b550eddc7e2f637f1ab1830
wally-wally <wallys0213@gmail.com>
2020-12-24 00:53:02 +0900

    201224 | revise convert file size util js function

--- 06_js/08_util_functions/convertFileSize.js
+++ 06_js/08_util_functions/convertFileSize.js
@@ -1,6 +1,6 @@
-export const converFileSize = (fileSize, roundDigit = 0) => {
+export const converFileSize = (fileSize, round = 0) => {
   // 계산하기 전 '0[Byte]'로 출력되는 경우
-  // (1) size 파라미터가 null이나 undefined인 경우 
+  // (1) size parameter가 null이나 undefined인 경우 
   if (fileSize === null || fileSize === undefined) {
     return '0[Byte]';
   }
@@ -12,6 +12,15 @@ export const converFileSize = (fileSize, roundDigit = 0) => {
   // (3) 파일 용량이 음수인 경우는 없으므로 음수 숫자가 넘어오면 '0[Byte]'로 고정
   if (size < 0) {
     return '0[Byte]';
+  }
+
+  // 반올림 자리수 결정
+  // round parameter가 숫자 형태이면서 1 이상인 경우에 입력된 parameter 사용
+  // 그 외의 경우는 0으로 고정
+  let roundDigit = 0;
+  const numberizeRound = parseInt(round);
+  if (!isNaN(numberizeRound) && numberizeRound >= 1) {
+    roundDigit = numberizeRound;
   }
 
   // 실제 용량 변환 계산 로직
