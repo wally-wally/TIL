2f5199080ba41faf097182845ea05838d07d1daf
wally-wally <wallys0213@gmail.com>
2020-12-26 15:55:36 +0900

    201226 | delete unnecessary files

--- 04_django/django_practice/accounts/forms.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        fields = UserCreationForm.Meta.fields + ('email',)
--- 04_django/django_practice/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'accounts'
-urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-]
