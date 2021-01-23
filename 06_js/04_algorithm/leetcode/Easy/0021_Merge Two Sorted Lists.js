383b969b074a1cadf20d907e4a24ca39e9367a06
wally-wally <wallys0213@gmail.com>
2021-01-23 16:24:46 +0900

    210123 | add leetcode js algorithm problem

---
+++ 06_js/04_algorithm/leetcode/Easy/0021_Merge Two Sorted Lists.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+class ListNode {
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+const mergeTwoLists = (l1, l2) => {
+  const checkRecursiveNode = (n1, n2, n3) => {
+    if (!n1 && !n2) {
+      return n3;
+    }
+
+    if (n1 && n2) {
+      if (n1.val <= n2.val) {
+        n3.next = new ListNode(n1.val);
+        return checkRecursiveNode(n1.next, n2, n3.next);
+      }
+      if (n1.val > n2.val) {
+        n3.next = new ListNode(n2.val);
+        return checkRecursiveNode(n1, n2.next, n3.next);
+      }
+    } else if (n1 && !n2) {
+      n3.next = new ListNode(n1.val);
+      return checkRecursiveNode(n1.next, null, n3.next);
+    } else if (!n1 && n2) {
+      n3.next = new ListNode(n2.val);
+      return checkRecursiveNode(null, n2.next, n3.next);
+    }
+  };
+
+  const l3 = new ListNode(null);
+  checkRecursiveNode(l1, l2, l3);
+  return l3.next;
+};
