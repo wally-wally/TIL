41874cc063ac32381ee658908251e1dba955d1f4
wally-wally <wallys0213@gmail.com>
2021-04-30 20:33:31 +0900

    210430 | add vue3 preview contents

---
+++ 07_vue/08_learn-vue-typescript/vue3-ts/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+// Vue 3 에서도 Vue 2 방식의 코드 구성 가능
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      str: "hi",
+    };
+  },
+  methods: {
+    changeStr() {
+      this.str += "!";
+    },
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 07_vue/08_learn-vue-typescript/vue3-ts/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    const str = ref("");
+
+    str.value = "hi";
+
+    return { str };
+  },
+
+  // 위 코드와 동일
+  // data() {
+  //   return {
+  //     str: "",
+  //   };
+  // },
+  // methods: {
+  //   changeString() {
+  //     this.str = "hi";
+  //   },
+  // },
+});
+</script>
+
+<style scoped></style>
