b925afdd8a31df0b808bee23b31fd9a1d031440b
Wally <sgh@gabia.com>
2023-02-20 00:27:22 +0900

    230220 | readonly, index signature, mapped type

--- 09_ts/05_ts-all-in-one/06.ts
+++ 09_ts/05_ts-all-in-one/06.ts
@@ -13,3 +13,22 @@ if (z) {
 } else {
   z;
 }
+
+// readonly: 읽기 전용 속성으로 속성 실수로 변경하는 거를 막아주는 역할을 함
+interface AAA {
+  readonly a: string;
+  b: string;
+}
+
+const aaaa: AAA = { a: 'hello', b: 'world' };
+// aaaa.a = '123';
+
+// 인덱스 시그니처
+type BBB = { a: number, b: number, c: number, d: number; };
+type BBBB = { [key: string]: number }
+const bbbb: BBBB = { a: 3, b: 4, c: 5, d: 6 };
+
+// 맵드 타입
+type CCC = 'Human' | 'Mammal' | 'Animal';
+type CCCC = { [key in CCC]: number };
+const cccc: CCCC = { Human: 123, Mammal: 5, Animal: 7 };
